8af8c9fb6e867710a41f4dd6e822b04ad2677340
GergelyOlah <olah.gergely.imre@gmail.com>
2020-12-28 21:06:50 +0000

    Assignent_12_3.py solved

--- Assigment_12_3.py
+++ Assigment_12_3.py
@@ -11,39 +11,22 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-def parser(url, pos, cnt):
-    print(url)
-    repeat = cnt - 1
-    if repeat = 1
-        next_url = None
-    else:
-        html = urllib.request.urlopen(url, context=ctx).read()
-        soup = BeautifulSoup(html, 'html.parser')
-        tags = soup('a')
-        line_number = 0
-    
-        for tag in tags:
-            line_number += 1
-            if line_number != pos:
-                continue
-            else:
-                next_url = tag['href']
-                parser(next_url,pos,repeat)
-
-        return next_url
+count = int(input("Enter count: "))
+position = int(input("Enter position: "))
 
 url = "http://py4e-data.dr-chuck.net/known_by_Fikret.html"
-html = urllib.request.urlopen(url, context=ctx).read()
-soup = BeautifulSoup(html, 'html.parser')
-tags = soup('a')
-line_number = 0
-#for tag in tags:
-    #print(tag.get('href', None))
-    #print(tag['href'])
 
+for i in range(count+1):
+    print("Retrieving: ", url)
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = soup('a')
+    line_number = 0
 
-#count = int(input("Enter count: "))
-#position = int(input("Enter position: "))
-
-#parser("http://py4e-data.dr-chuck.net/known_by_Fikret.html", position)
-  
+    for tag in tags:
+        line_number += 1
+        if line_number != position:
+            continue
+        else:
+            url = tag['href']
+ 
