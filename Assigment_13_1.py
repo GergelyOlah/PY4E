29dfba849cc5e63d559bb171902b02a1036ff83f
GergelyOlah <olah.gergely.imre@gmail.com>
2020-12-29 17:10:02 +0000

    Assignment 13 refactored

--- Assigment_13_1.py
+++ Assigment_13_1.py
@@ -3,20 +3,26 @@ import xml.etree.ElementTree as ET
 import ssl
 
 # Getting inputs:
-""" url = input("Enter URL: ")
-if url == "": """
-url = "http://py4e-data.dr-chuck.net/comments_42.xml"
+url = input("Enter URL: ")
+if url == "":
+    url = "http://py4e-data.dr-chuck.net/comments_42.xml"
 
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
+# Retriving URL:
 xml = urllib.request.urlopen(url, context=ctx)
 data = xml.read()
-tree = ET.fromstring(data)
 
+# Creating an XML tree
+tree = ET.fromstring(data) 
+
+# Xreating a list of the below tags:
 results = tree.findall('comments/comment')
+
+# Parsing through the list and extract the text:
 sums = 0
 count = 0
 
@@ -25,8 +31,11 @@ for tag in results:
     number = int(tag.find("count").text)
     sums += number
 
-print(count, sums)
-
+print("Retrieving", url)
+print("Data type:", type(data))
+print("Retrieved {} characters".format(len(data)))
+print("Count: ", count)
+print("Sum: ", sums)
 
 
 
