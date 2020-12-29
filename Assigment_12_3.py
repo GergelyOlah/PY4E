e49e1cfe6727bb01da8c981ab942892e7bfe71bd
GergelyOlah <olah.gergely.imre@gmail.com>
2020-12-29 09:15:43 +0000

    Name printed as answer

--- Assigment_12_3.py
+++ Assigment_12_3.py
@@ -11,19 +11,21 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-# Spider through the links:
+# Getting inputs:
+url = input("Enter URL: ")
+if url == "":
+    url = "http://py4e-data.dr-chuck.net/known_by_Fikret.html"
+
 count = int(input("Enter count: "))
 position = int(input("Enter position: "))
 
-url = "http://py4e-data.dr-chuck.net/known_by_Fikret.html"
+# Spider through the links:
+print("Retrieving: ", url)
 
-for i in range(count+1):
-    print("Retrieving: ", url)
-    
+for i in range(count):
     html = urllib.request.urlopen(url, context=ctx).read()
     soup = BeautifulSoup(html, 'html.parser')
     tags = soup('a')
-    name = tags[position].contents[0]
     line_number = 0
 
     for tag in tags:
@@ -32,3 +34,7 @@ for i in range(count+1):
             continue
         else:
             url = tag['href']
+            print("Retrieving: ", url)
+            name = tag.contents[0]
+
+print(name)
