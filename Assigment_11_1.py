0522830c6a010017c4e7b54f48c82ea4b0b58d92
GergelyOlah <olah.gergely.imre@gmail.com>
2020-12-29 11:38:07 +0000

    Counting function added

--- Assigment_11_1.py
+++ Assigment_11_1.py
@@ -7,13 +7,15 @@ if file == "":
     file = "regex_sum_42.txt"
 
 with open(file) as fhand:
-    num_list = []
     count = 0
+    sum_all = 0
+
     for line in fhand:
         line = line.rstrip()
         extracted_strings = re.findall("[0-9]+", line)
+        count += len(extracted_strings)
         extracted_numbers = [int(i) for i in extracted_strings]
         sum_extracted_numbers = sum(extracted_numbers)
-        count += sum_extracted_numbers
+        sum_all += sum_extracted_numbers
     
-    print(count)
+print("There are {} numbers with a total sum: {}".format(count, sum_all))
