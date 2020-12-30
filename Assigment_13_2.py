71ac08ec17d0b3f1b4ca8c46d2816c31e4a66955
GergelyOlah <olah.gergely.imre@gmail.com>
2020-12-30 14:51:21 +0000

    Assigment_13_3.py added

---
+++ Assigment_13_2.py
@@ -0,0 +1,35 @@
+import urllib.request, urllib.parse, urllib.error
+import json
+import ssl
+
+# Getting inputs:
+url = input("Enter URL: ")
+if url == "":
+    url = "http://py4e-data.dr-chuck.net/comments_42.json"
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+# Retriving URL:
+print("Retrieving", url)
+json_page = urllib.request.urlopen(url, context=ctx)
+raw_data = json_page.read().decode() #"bytes" data is decoded to "string"
+
+# Turning JSON to Python data structure:
+json_data = json.loads(raw_data)
+print("Retrieved {} characters".format(len(raw_data)))
+print("Raw data type:\n", type(raw_data))
+print("Json data type:\n", type(json_data))
+
+# Parsing through the list and extract the text:
+sums = 0
+count = 0
+
+for user in json_data["comments"]:
+    count += 1
+    sums += int(user["count"])
+    
+print("Count: ", count)
+print("Sum: ", sums)
---
+++ Assigment_13_3.py
@@ -0,0 +1,44 @@
+import urllib.request, urllib.parse, urllib.error
+import json
+import ssl
+
+# API:
+api_key = 42
+serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+# Create URL:
+address = input('Enter location: ')
+
+parms = dict()
+parms['address'] = address
+parms['key'] = api_key
+url = serviceurl + urllib.parse.urlencode(parms)
+
+# Retrieving URL:
+print('Retrieving', url)
+uh = urllib.request.urlopen(url, context=ctx)
+raw_data = uh.read().decode()
+print('Retrieved', len(raw_data), 'characters')
+
+# Convert webpage to JSON data:
+try:
+    json_data = json.loads(raw_data)
+except:
+    json_data = None
+
+if not json_data or 'status' not in json_data or json_data['status'] != 'OK':
+    print('==== Failure To Retrieve ====')
+    print(raw_data)
+
+# Convert JSON data to Python data to pretty print it:
+print(json.dumps(json_data, indent=4))
+
+# Extract place id:
+place_id = json_data['results'][0]['place_id']
+
+print("Place id:\n", place_id)
