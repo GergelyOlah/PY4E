5e2864bdf15a60e6c2cd733e0c84fdbcd3c1732d
GergelyOlah <olah.gergely.imre@gmail.com>
2021-01-05 21:01:21 +0000

    Comments added to code

--- code3/code3/urllinks.py
+++ code3/code3/urllinks.py
@@ -12,10 +12,10 @@ ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
 url = input('Enter - ')
-html = urllib.request.urlopen(url, context=ctx).read()
+html = urllib.request.urlopen(url, context=ctx).read() # The read() returns the entire webpage as a huge string (with newlines at the end of each line) instead of an iterable filehandle
 soup = BeautifulSoup(html, 'html.parser')
 
 # Retrieve all of the anchor tags
-tags = soup('a')
+tags = soup('a') #Gives back a list of <a> </a> tags.
 for tag in tags:
-    print(tag.get('href', None))
+    print(tag.get('href', None)) # Prints the content of the "href"attribute in the <a> tags
